7da8f7c573b16c170e8ac0d747a5ec80dcc0a540
Johny Kidd <kidnixt@gmail.com>
2022-10-31 06:58:17 -0300

    ola

---
+++ index.js
@@ -0,0 +1,13 @@
+var name = "yoni"
+        console.log("hola")
+
+    let edad = 21;
+                        edad++;
+
+function saludar(name){
+            console.log(name);
+}
+
+function saludar2(){
+      console.long("hola");
+}
