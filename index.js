b5b39ab7b620e8713178d88f8cafd56e9d3e6571
Johny Kidd <57159622+kidnixt@users.noreply.github.com>
2025-05-25 23:17:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-var name = "yoni"
-        console.log("hola")
+var nombre = "Juan" 
 
-    let edad = 21;
-                        edad++;
+var edad = 30 
 
-function saludar(name){
-            console.log(name);
+alert("¡Hola!"); 
+
+const PI = 3.14
+PI = 3.1416 
+
+function calcular(valor) {
+  if (valor > 0) {
+    if (valor > 10) {
+      if (valor > 20) {
+        if (valor > 30) {
+          console.log("Valor muy grande")
+        }
+      }
+    }
+  }
+
+  let a = 1
+  let b = 2
+  let c = 3
+  let d = 4
+  let e = 5
+  let f = 6
+  let g = 7
+  let h = 8
+  let i = 9
+  let j = 10
+  let k = 11
+  let l = 12
+  let m = 13
+  let n = 14
+  let o = 15
+  let p = 16 
+
+  return a + b + c
 }
 
-function saludar2(){
-      console.long("hola");
+function sinUso(x, y) {
+  let resultado = x + y
+}
+
+const datos = {
+  nombre:"Ana",apellido:"Pérez",edad:25, 
+}
+
+console
+.log("separado mal") 
+
+undeclarada = 5 
+
+label:
+for (var i = 0; i < 3; i++) {
+  break label 
 }
