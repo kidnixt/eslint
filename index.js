b09e5b9e13c400edd958d605071b11f33a55c6bc
FACULTADES\alumnoFI <alumnoFI>
2025-05-26 09:05:30 -0300

    eslint 2k25

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-var nombre = "Juan" 
+let nombre = "Juan";
 
-var edad = 30 
+let edad = 30;
 
-alert("¡Hola!"); 
+alert("¡Hola!");
 
-const PI = 3.14
-PI = 3.1416 
+const PI = 3.14;
+PI = 3.1416;
 
 function calcular(valor) {
   if (valor > 0) {
     if (valor > 10) {
       if (valor > 20) {
         if (valor > 30) {
-          console.log("Valor muy grande")
+          console.log("Valor muy grande");
         }
       }
     }
   }
 
-  let a = 1
-  let b = 2
-  let c = 3
-  let d = 4
-  let e = 5
-  let f = 6
-  let g = 7
-  let h = 8
-  let i = 9
-  let j = 10
-  let k = 11
-  let l = 12
-  let m = 13
-  let n = 14
-  let o = 15
-  let p = 16 
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  let d = 4;
+  let e = 5;
+  let f = 6;
+  let g = 7;
+  let h = 8;
+  let i = 9;
+  let j = 10;
+  let k = 11;
+  let l = 12;
+  let m = 13;
+  let n = 14;
+  let o = 15;
+  let p = 16;
 
-  return a + b + c
+  return a + b + c;
 }
 
 function sinUso(x, y) {
-  let resultado = x + y
+  let resultado = x + y;
 }
 
 const datos = {
-  nombre:"Ana",apellido:"Pérez",edad:25, 
-}
+  nombre: "Ana",
+  apellido: "Pérez",
+  edad: 25,
+};
 
-console
-.log("separado mal") 
+console.log("separado mal");
 
-undeclarada = 5 
+undeclarada = 5;
 
-label:
-for (var i = 0; i < 3; i++) {
-  break label 
+label: for (let i = 0; i < 3; i++) {
+  break label;
 }
